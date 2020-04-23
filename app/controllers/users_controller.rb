bf5a635d85ed4d159a08f58d73d3986cd080332a
alexandre jamarco <alexandre.j@gmail.com>
2020-04-24 00:02:01 +0100

    local storage working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def sign_in
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            render json: {message: "success"}
+            render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
         else
             render json: {message: "failure"}
         end
