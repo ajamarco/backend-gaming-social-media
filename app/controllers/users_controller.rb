d9b4ff5d77f038c50041488fbf46e458be5f7288
alexandre jamarco <alexandre.j@gmail.com>
2020-04-24 11:07:04 +0100

    created requests in a different file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
             render json: {message: "failure"}
         end
     end
+
+    def validate
+        id = decode_token
+        user = User.find_by(id: id)
+        if user
+            render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
+        else
+            render json: {message: "failure"}
+        end
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   
   post "/sign-in", to: "users#sign_in"
+  get "/validate", to: "users#validate"
 end
