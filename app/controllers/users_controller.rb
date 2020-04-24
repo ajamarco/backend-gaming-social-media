deac7431612907304f5e12518f78497c7d65e11f
alexandre jamarco <alexandre.j@gmail.com>
2020-04-24 18:24:35 +0100

    removed byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def sign_in
         user = User.find_by(email: params[:email])
+        
         if user && user.authenticate(params[:password])
             render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
         else
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     def validate
         id = decode_token["id"]
         user = User.find_by(id: id)
-        # byebug
+       
         if user
             render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
         else
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# u1 = User.create(email: "alex@gmail.com", password_digest: "123")
-# u2 = User.create(email: "test1@email.com", password_digest: "123")
-# u3 = User.create(email: "test2@email.com", password_digest: "123")
-# u4 = User.create(email: "test3@email.com", password_digest: "123")
-# u5 = User.create(email: "test4@email.com", password_digest: "345")
+u1 = User.create(email: "alex@gmail.com", password: "123")
+u2 = User.create(email: "test1@email.com", password: "123")
+u3 = User.create(email: "test2@email.com", password: "123")
+u4 = User.create(email: "test3@email.com", password: "123")
+u5 = User.create(email: "test4@email.com", password: "345")
 
 
 Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 4)
