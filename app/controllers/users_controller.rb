ebb5deb42d1601317ee5b78bbb655c00c0788643
alexandre jamarco <alexandre.j@gmail.com>
2020-04-26 16:07:58 +0100

    added serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
-    def sign_in
-        user = User.find_by(email: params[:email])
-        
-        if user && user.authenticate(params[:password])
-            render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
+    def sign_in(user = "")
+        #if user is login in, which means the params are in the request
+        if (user == "")
+            user = User.find_by(email: params[:email])
+            if user && user.authenticate(params[:password])
+                render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
+            else
+                render json: {message: "failure"}
+            end
+        #if a user just sign up and will authenticate it
         else
-            render json: {message: "failure"}
+            if user && user.authenticate(user.password)
+                render json: {message: "success", email: user.email, token: generate_token(id: user.id) }
+            else
+                render json: {message: "failure"}        
+            end
+        #     #TODO validation and error messages if the information was wrong. Treat errors here and in the frontend. It should use the helper text. 5:43:30
         end
     end
+
+    def create
+        user = User.new(user_params)
+        #TODO verify if password matches // email is unique, error handling
+        if user.save
+            sign_in(user)
+        else
+            render json: {status: 500, message: user.errors}
+        end
+    end
+
 
     def validate
         id = decode_token["id"]
@@ -23,6 +44,13 @@ class UsersController < ApplicationController
     def index
         users = User.all
         render json: users
+    end
+
+
+    private
+
+    def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   has_many :comments
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
