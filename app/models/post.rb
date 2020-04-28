225e2c4a15d181481a62c4e57c6cc54e9379ff05
alexandre jamarco <alexandre.j@gmail.com>
2020-04-28 11:32:55 +0100

    added method to return posts id that user liked

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :likes
+
+  def get_users_that_commented
+    #if we dont want the email (just the id) we remove the last map
+    self.comments.map { |c| c.user_id}.map{|u| User.find(u).email}
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   has_many :comments
+  has_many :likes
 
   validates :email, presence: true
   validates :email, uniqueness: true
+
+  def get_users_likes
+    #if we dont want the email (just the id) we remove the last map
+    self.likes.map { |c| c.post_id}
+  end
 end
