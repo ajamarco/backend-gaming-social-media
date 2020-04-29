94bc106a3b871309a95423bf3f974c8790a8d06d
alexandre jamarco <alexandre.j@gmail.com>
2020-04-29 11:54:25 +0100

    fixed delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,18 @@ class PostsController < ApplicationController
         render json: posts
     end
 
+    def create
+        content = params[:content]
+        user_id = params[:user_id]
+
+        post = Post.new(content: content, user_id: user_id, number_of_likes: 0)
+        if post.save
+            render json: post
+        else
+            render json: {status: 'ERROR', message:'post not saved', data:post.errors},status: :unprocessable_entity
+        end
+    end
+
     def show
         post = Post.find(params[:id])
         if post
@@ -14,7 +26,7 @@ class PostsController < ApplicationController
     end
 
     def destroy
-
+        # byebug
         post = Post.find(params[:id])
         post.destroy
         render json: {status: 'SUCCESS', message:'Deleted article', data: post.id},status: :ok
