db0ffb665f1d6f4343bf732c17c74b6e49034372
alexandre jamarco <alexandre.j@gmail.com>
2020-05-03 15:03:39 +0100

    added comment serializer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+    def create
+        content = params[:content]
+        user_id = params[:user_id]
+        post_id = params[:post_id]
+        # byebug
+        comment = Comment.new(content: content, user_id: user_id, post_id: post_id )
+        if comment.save
+            render json: comment
+        else
+            render json: {status: 'ERROR', message:'comment not saved', data:comment.errors},status: :unprocessable_entity
+        end
+    end
 end
