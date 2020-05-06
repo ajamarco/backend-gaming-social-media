ae850b4e08b9a482f947fa0a8fa5b8c973f194da
alexandre jamarco <alexandre.j@gmail.com>
2020-05-06 18:57:21 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# u1 = User.create(email: "alex@gmail.com", password: "123")
-# u2 = User.create(email: "test1@email.com", password: "123")
-# u3 = User.create(email: "test2@email.com", password: "123")
-# u4 = User.create(email: "test3@email.com", password: "123")
-# u5 = User.create(email: "test4@email.com", password: "345")
+u1 = User.create(email: "alex@gmail.com", password: "123")
+u2 = User.create(email: "test1@email.com", password: "123")
+u3 = User.create(email: "test2@email.com", password: "123")
+u4 = User.create(email: "test3@email.com", password: "123")
+u5 = User.create(email: "test4@email.com", password: "345")
 
 
-# Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 4)
-# Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 5)
-# Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 1)
-# Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 3)
-# Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 2)
+Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 4)
+Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 5)
+Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 1)
+Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 3)
+Post.create(content: Faker::Lorem.paragraph, number_of_likes: Faker::Number.number(digits= 2), user_id: 2)
 
 
-# 10.times do
-#     Comment.create(content: Faker::Lorem.paragraph, post_id: Faker::Number.between(from= 1, to= Post.all.length), user_id: Faker::Number.between(from= 1, to= User.all.length))
-# end
+10.times do
+    Comment.create(content: Faker::Lorem.paragraph, post_id: Faker::Number.between(from= 1, to= Post.all.length), user_id: Faker::Number.between(from= 1, to= User.all.length))
+end
 
 Like.create(user_id: 1, post_id: 1)
 Like.create(user_id: 1, post_id: 2)
