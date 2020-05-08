e7b15dc540f30052171c35e93dce9063157c5a04
alexandre jamarco <alexandre.j@gmail.com>
2020-05-08 16:07:00 +0100

    added img_url to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :bio, :location, :website, :posts
+  attributes :id, :email, :created_at, :bio, :location, :website, :img_url, :posts
 
   def posts
     self.object.posts
